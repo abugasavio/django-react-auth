3ee021b53bf41d7929f6dd9d394e0b62e46a6c89
savioabuga <savioabuga@gmail.com>
2016-03-25 10:46:08 +0300

    updating api application

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, response, permissions
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated)
+
+    def retrieve(self, request, pk=None):
+        if pk == 'i':
+            return response.Response(UserSerializer(request.user, context={'request': request}).data)
+        return super(UserViewSet, self).retrieve(request, pk)
--- django_react_auth/settings.py
+++ django_react_auth/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    # internal apps
     'api',
 ]
 
